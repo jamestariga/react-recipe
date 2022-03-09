22231e8d45cf858a2c752a220c0365e0e99da296
James <jamestariga08@live.com>
2022-03-08 22:11:34 -0800

    ✨ First Commit

---
+++ src/Components/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Recipe from './Recipe'
+
+function App() {
+  return (
+    <>
+      <Recipe />
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Recipe.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Recipe = () => {
+  return (
+    <h1>Hello</h1>
+  )
+}
+
+export default Recipe
