81b34e4ea02592ef3c7b7719dab659050db3ea5a
James <jamestariga08@live.com>
2022-03-22 22:34:52 -0700

    ✨ Added a Footer section

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,35 @@
+import { IconContext } from 'react-icons'
+import { AiOutlineGithub } from 'react-icons/ai'
+import { BsLinkedin } from 'react-icons/bs'
+import { SiDevpost } from 'react-icons/si'
+
+const Footer = () => {
+  return (
+    <>
+      <IconContext.Provider value={{ size: '30px' }}>
+        <footer className='text-center bg-teal-600 text-white'>
+          <div className='w-full px-6 py-6'>
+            <div className='flex justify-center items-center'>
+              <div className='flex justify-evenly w-1/2'>
+                <a href='https://github.com/jamestariga'>
+                  <AiOutlineGithub color='#000' />
+                </a>
+                <a href='https://www.linkedin.com/in/james-tariga/'>
+                  <BsLinkedin color='#035094' />
+                </a>
+                <a href='https://devpost.com/jamestariga'>
+                  <SiDevpost color='#003E54' />
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className='text-center bg-teal-700 py-2'>
+            <p>© 2021 Copyright: James Tariga</p>
+          </div>
+        </footer>
+      </IconContext.Provider>
+    </>
+  )
+}
+
+export default Footer
