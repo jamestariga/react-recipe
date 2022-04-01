524bb97df7c4c88fdac45d000bfbfa58f2081386
James <jamestariga08@live.com>
2022-04-01 15:17:17 -0700

    ✨ Added animated page transition

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,20 +1,14 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import Recipe from './Recipe/Recipe'
+import { BrowserRouter as Router } from 'react-router-dom'
 import Navbar from './Navbar/Navbar'
-import Home from './Home/Home'
-import Search from './Search/Search'
 import Footer from './Footer/Footer'
+import AnimatedRoutes from './Router/AnimatedRoutes'
 
 const App = () => {
   return (
     <>
       <Router>
         <Navbar />
-        <Routes>
-          <Route path='/' element={<Home />} exact />
-          <Route path='/recipe' element={<Recipe />} />
-          <Route path='/search' element={<Search />} />
-        </Routes>
+        <AnimatedRoutes />
         <Footer />
       </Router>
     </>
---
+++ src/Components/Router/AnimatedRoutes.js
@@ -0,0 +1,22 @@
+import { Route, Routes, useLocation } from 'react-router-dom'
+import Recipe from '../Recipe/Recipe'
+import Search from '../Search/Search'
+import Home from '../Home/Home'
+import { AnimatePresence } from 'framer-motion'
+
+const AnimatedRoutes = () => {
+  const location = useLocation()
+  return (
+    <>
+      <AnimatePresence>
+        <Routes location={location} key={location.pathname}>
+          <Route path='/' element={<Home />} exact />
+          <Route path='/recipe' element={<Recipe />} />
+          <Route path='/search' element={<Search />} />
+        </Routes>
+      </AnimatePresence>
+    </>
+  )
+}
+
+export default AnimatedRoutes
